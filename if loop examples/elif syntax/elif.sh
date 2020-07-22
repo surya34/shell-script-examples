4784b272295c61e3534972103648f37bc342d969
surya34 <67806591+surya34@users.noreply.github.com>
2020-07-21 19:11:45 -0500

    Update elif.sh

--- if loop examples/elif syntax/elif.sh
+++ if loop examples/elif syntax/elif.sh
@@ -1,1 +1,10 @@
 #!/bin/bash 
+if [ $1 -gt 0 ] ; then 
+echo " $1 is postive  " 
+elif [ $1 -lt 0 ] ; then
+echo " $1 id negative " 
+elif [ $1 -eq 0 ] ; then 
+echo " $1 equal to zero " 
+else 
+echo " $1 is not a number  "
+fi 
