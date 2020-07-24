0311bfa76fb1a71297309489169968de4fe263bd
surya34 <67806591+surya34@users.noreply.github.com>
2020-07-23 23:09:43 -0500

    Create memory.sh

---
+++ if loop examples/memory.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# script to get the memory , hard disk information like size of hard-disk, cache memory, model , File system (Mounted)
+
+# cpu information like processor type, speed 
+
