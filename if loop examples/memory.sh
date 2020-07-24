c338b4e7b9342242dcb5166d1cc1c5d14baadbc3
surya34 <67806591+surya34@users.noreply.github.com>
2020-07-23 23:12:33 -0500

    Update memory.sh

--- if loop examples/memory.sh
+++ if loop examples/memory.sh
@@ -4,3 +4,12 @@
 
 # cpu information like processor type, speed 
 
+if [ $# -eq 0 ] ; then 
+echo "print the memory info"
+free -g
+else
+ if [ $# -eq 1 ] ; then 
+ echo "print storage info "
+ fi
+fi 
+
